9ab0e2e45a5f790620c2b5de8c0cf6c698db2c91
MdAsraful56 <work.mdasraful56@gmail.com>
2025-02-17 23:32:01 +0600

    BgChanger project completed

--- BgChanger/src/App.jsx
+++ BgChanger/src/App.jsx
@@ -11,7 +11,14 @@ function App() {
         <h1 className='text-4xl text-center p-5 text-orange-50'>Background Color Changer</h1>
         <div className='flex flex-wrap fixed justify-center items-center bottom-12 inset-x-0 px-2'>
           <div className="flex flex-wrap justify-center gap-4 bg-white px-3 py-2 shadow-2xl rounded-2xl">
-            <button style={{backgroundColor: 'red'}} className="btn outline-none px-4 py-1 rounded-lg text-white shadow-lg">Red</button>
+            <button onClick={()=> setColor('red')} style={{backgroundColor: 'red'}} className="btn outline-none px-4 py-1 rounded-lg text-black shadow-lg">Red</button>
+            <button onClick={()=> setColor('green')} style={{backgroundColor: 'green'}} className="btn outline-none px-4 py-1 rounded-lg text-black shadow-lg">Green</button>
+            <button onClick={()=> setColor('blue')} style={{backgroundColor: 'blue'}} className="btn outline-none px-4 py-1 rounded-lg text-black shadow-lg">Blue</button>
+            <button onClick={()=> setColor('gray')} style={{backgroundColor: 'gray'}} className="btn outline-none px-4 py-1 rounded-lg text-black shadow-lg">Gray</button>
+            <button onClick={()=> setColor('yellow')} style={{backgroundColor: 'yellow'}} className="btn outline-none px-4 py-1 rounded-lg text-black shadow-lg">Yellow</button>
+            <button onClick={()=> setColor('pink')} style={{backgroundColor: 'pink'}} className="btn outline-none px-4 py-1 rounded-lg text-black shadow-lg">Pink</button>
+            <button onClick={()=> setColor('white')} style={{backgroundColor: 'white'}} className="btn outline-none px-4 py-1 rounded-lg text-black shadow-lg">White</button>
+            <button onClick={()=> setColor('lavender')} style={{backgroundColor: 'lavender'}} className="btn outline-none px-4 py-1 rounded-lg text-black shadow-lg">lavender</button>
           </div>
         </div>
       </div>
