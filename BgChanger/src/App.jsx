801d5c5f5de0f2117f4ddfa4794ac8f7a0860eb0
MdAsraful56 <work.mdasraful56@gmail.com>
2025-02-17 23:25:22 +0600

    BgChanger Done 1 Color Complete

--- BgChanger/src/App.jsx
+++ BgChanger/src/App.jsx
@@ -1,12 +1,19 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [color, setColor] = useState('black');
   
 
   return (
     <>
-      <div className="">
-        <h1>Background Color Changer</h1>
+      <div className="h-screen w-full duration-150" style={{backgroundColor: color}}>
+        <h1 className='text-4xl text-center p-5 text-orange-50'>Background Color Changer</h1>
+        <div className='flex flex-wrap fixed justify-center items-center bottom-12 inset-x-0 px-2'>
+          <div className="flex flex-wrap justify-center gap-4 bg-white px-3 py-2 shadow-2xl rounded-2xl">
+            <button style={{backgroundColor: 'red'}} className="btn outline-none px-4 py-1 rounded-lg text-white shadow-lg">Red</button>
+          </div>
+        </div>
       </div>
     </>
   )
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # React-With-Chai-New
+
+
+### Video - 09: BgChanger ==> Click to btn Change Background color ==> Using useState && onClick 
